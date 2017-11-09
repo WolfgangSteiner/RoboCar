24e25b50c5cffa0f6f45365ebe69c171e0fdc940
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2017-11-09 11:57:53 +0100

    add stop signal to servo driver

--- ros/arduino/servo_driver/servo_driver.ino
+++ ros/arduino/servo_driver/servo_driver.ino
@@ -1,5 +1,6 @@
 #include <ros.h>
 #include <std_msgs/Int16.h>
+#include <std_msgs/Bool.h>
 #include <Servo.h>
 
 ros::NodeHandle node_handle;
@@ -8,27 +9,44 @@ Servo steering, throttle;
 const int KMaxSteer = 400;
 const int KMinThrottle = -200;
 const int KMaxThrottle = 200;
+bool stop_value = false;
 
 
 void steer_callback(const std_msgs::Int16& msg)
 {
-   const int in_value = msg.data;
-   const int out_value = constrain(in_value, 1500 - KMaxSteer, 1500 + KMaxSteer);
-   steering.writeMicroseconds(out_value);
+  if (!stop_value)
+  {
+    const int in_value = msg.data;
+    const int out_value = constrain(in_value, 1500 - KMaxSteer, 1500 + KMaxSteer);
+    steering.writeMicroseconds(out_value);
+  }
 }
 
 
 void throttle_callback(const std_msgs::Int16& msg)
 {
-   const int in_value = msg.data;
-   const int out_value = constrain(in_value, 1500 + KMinThrottle, 1500 + KMaxThrottle);
-   throttle.writeMicroseconds(out_value);
-}  
+  if (!stop_value)
+  {
+    const int in_value = msg.data;
+    const int out_value = constrain(in_value, 1500 + KMinThrottle, 1500 + KMaxThrottle);
+    throttle.writeMicroseconds(out_value);
+  }
+}
 
+
+void stop_callback(const std_msgs::Bool& msg)
+{
+  stop_value = msg.data;
+  if (stop_value)
+  {
+    steering.writeMicroseconds(1500);
+    throttle.writeMicroseconds(1500);
+  }
+}
 
 ros::Subscriber<std_msgs::Int16> steering_sub("/steering_value_us", &steer_callback);
 ros::Subscriber<std_msgs::Int16> throttle_sub("/throttle_value_us", &throttle_callback);
-
+ros::Subscriber<std_msgs::Bool> stop_sub("/stop_signal", &stop_callback);
 
 void setup()
 {
@@ -39,6 +57,7 @@ void setup()
   throttle.writeMicroseconds(1500);
   
   node_handle.initNode();
+  node_handle.subscribe(stop_sub);
   node_handle.subscribe(steering_sub);
   node_handle.subscribe(throttle_sub);
 }

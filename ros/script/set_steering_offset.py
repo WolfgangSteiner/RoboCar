9b3ac1d55f0ed2f6df67993f40660199d8dfb4e0
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2018-09-04 14:16:06 +0200

    add scripts to set steering angle and offset

---
+++ ros/script/set_steering_offset.py
@@ -0,0 +1,38 @@
+#! /usr/bin/python
+
+import argparse
+import os
+import sys
+import rospy
+from std_msgs.msg import Float32
+
+parser = argparse.ArgumentParser()
+parser.add_argument("direction", type=str)
+args = parser.parse_args()
+
+if args.direction == "left":
+    delta = -1.0
+elif args.direction == "right":
+    delta = 1.0
+elif args.direction == "center":
+    delta = 0.0
+else:
+    print("Invalid direction argument")
+    exit()
+
+
+def on_shutdown():
+    pub.publish(0.0)
+      
+rospy.on_shutdown(on_shutdown)
+
+if __name__=='__main__':
+    rospy.init_node('steering_setter')
+    pub=rospy.Publisher('/steering_offset', Float32, queue_size=1)
+    rate=rospy.Rate(10)
+    
+    while not rospy.is_shutdown():
+        pub.publish(delta)
+        rate.sleep()
+        
+    pub.publish(0.0) 
